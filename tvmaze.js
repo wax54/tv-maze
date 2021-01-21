f0c1e5544efb04c8c26e0b938b9220459addb3ee
sam <wax654@gmail.com>
2021-01-20 19:50:47 -0800

    wrote out search shows

--- tvmaze.js
+++ tvmaze.js
@@ -1,3 +1,5 @@
+const GENERIC_URL = 'generic_tv.png'
+
 /** Given a query string, return array of matching shows:
  *     { id, name, summary, episodesUrl }
  */
@@ -5,7 +7,7 @@
 
 /** Search Shows
  *    - given a search term, search for tv shows that
- *      match that query.  The function is async show it
+ *      match that query.  The function is async so it
  *       will be returning a promise.
  *
  *   - Returns an array of objects. Each object should include
@@ -20,15 +22,30 @@
 async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
   // hard coded data.
+  try {
+    const res = await axios.get('http://api.tvmaze.com/search/shows',
+      {
+        params: {
+          q: query
+        }
+      });
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+    const rawShows = res.data;
+    const shows = rawShows.map(({ show }) => {
+      if (!show.image) {
+        show.image = {};
+        show.image.medium = GENERIC_URL;
+      }
+      const { id, name, summary, image: { medium: image } } = show;
+      return { id, name, summary, image }
+    });
+    return shows;
+
+  }
+  catch (e) {
+    console.log('Error On Search', e);
+    return;
+  }
 }
 
 
